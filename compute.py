54898b0afb5e7db2ce22905731c511b60edf1ea5
Evan <oakeef@gmail.com>
2026-07-16 18:24:19 -0300

    create test data, loop through test data

--- compute.py
+++ compute.py
@@ -4,3 +4,10 @@ threshold = sys.argv[1]
 limit = sys.argv[2]
 
 print(f"threshold: {threshold}, limit: {limit}")
+
+for line in sys.stdin:
+    if not line:
+        continue
+
+    
+    print(f"value: {line}")
